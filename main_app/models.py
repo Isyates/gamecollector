05e9789608172b6f058b5159c04b907be7eeabb8
Isiah Yates <isyates@outlook.com>
2022-07-18 02:19:15 -0400

    adds configuration for game collection

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Game:  # Note that parens are optional if not inheriting from another class
+  def __init__(self, title, genre, description, ryear):
+    self.title = title
+    self.genre = genre
+    self.description = description
+    self.ryear = ryear
+
+games = [
+  Game('Dark Souls', 'Action RPG', 'The game that started the "SoulsLike" subgenre', 2011),
+  Game('Sim City 4', 'Simulation', 'The game that brought city building back to the mainstream', 2003),
+  Game('League of Legends', 'MOBA', 'Built the idea of esports to what it is today', 2009)
+]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello Home</h1>')
+
+# About Route
+def about(request):
+  return render(request, 'about.html')
+
+# Games Index Route
+def games_index(request):
+  return render(request, 'games/index.html', { 'games': games })
