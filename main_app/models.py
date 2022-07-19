36328b45eeab152637431ed75e7254b52633b3e3
Isiah Yates <isyates@outlook.com>
2022-07-19 00:35:06 -0400

    adds detail view and modifies views and models

--- main_app/models.py
+++ main_app/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Game:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, title, genre, description, ryear):
-    self.title = title
-    self.genre = genre
-    self.description = description
-    self.ryear = ryear
+class Game(models.Model):  # Note that parens are optional if not inheriting from another class
+    title = models.CharField(max_length=250)
+    genre = models.CharField(max_length=250)
+    description = models.CharField(max_length=250)
+    ryear = models.IntegerField()
 
-games = [
-  Game('Dark Souls', 'Action RPG', 'The game that started the "SoulsLike" subgenre', 2011),
-  Game('Sim City 4', 'Simulation', 'The game that brought city building back to the mainstream', 2003),
-  Game('League of Legends', 'MOBA', 'Built the idea of esports to what it is today', 2009)
-]
+    def __str__(self):
+      return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Game
+
+
 
 # Define the home view
 def home(request):
-  return HttpResponse('<h1>Hello Home</h1>')
+  return render(request, 'home.html')
 
 # About Route
 def about(request):
@@ -11,4 +13,9 @@ def about(request):
 
 # Games Index Route
 def games_index(request):
-  return render(request, 'games/index.html', { 'games': games })
+  games = Game.objects.all()
+  return render(request, 'games/index.html', {'games': games})
+
+def games_detail(request, game_id):
+  game = Game.objects.get(id=game_id)
+  return render(request, 'games/detail.html', {'game': game})
